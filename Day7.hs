fec4c2db0883faba6c9424eb275c0d7c02336c67
vikrem <vikrem@users.noreply.github.com>
2021-12-07 12:23:54 -0500

    list comprehension

--- Day7.hs
+++ Day7.hs
@@ -13,10 +13,15 @@ day7 inputFile = do
   print $ solve2 input
 
 solve :: (Num a, Ord a) => [a] -> a
-solve xs = sum $ fmap (\x -> abs $ x - median xs) xs
+solve xs = sum $ [abs $ x - median xs | x <- xs]
 
-solve2 :: (Foldable t, Functor t, Integral a) => t a -> a
-solve2 xs = minimum $ fmap (\target -> sum $ fmap (\x -> rangeSum $ abs $ x - target) xs) [minimum xs .. maximum xs]
+solve2 :: Integral a => [a] -> a
+solve2 xs =
+  minimum $
+    [ sum $
+        [rangeSum $ abs $ x - target | x <- xs]
+      | target <- [minimum xs .. maximum xs]
+    ]
 
 median :: Ord a => [a] -> a
 median xs = sort xs !! (length xs `div` 2)
