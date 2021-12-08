c5440a62a511f78c0964f778f1c0793031ee7b75
vikrem <vikrem@users.noreply.github.com>
2021-12-08 08:50:59 -0500

    d8p2

--- Day8.hs
+++ Day8.hs
@@ -1,5 +1,8 @@
 module Day8 where
 
+import Control.Monad (guard)
+import Data.List (foldl', permutations, sort)
+import qualified Data.Map.Strict as M
 import Util (splitWhen)
 
 day8 :: FilePath -> IO ()
@@ -7,6 +10,9 @@ day8 inputFile = do
   content <- readFile inputFile
   let linedOutput = fmap (fmap words . splitWhen (== '|')) $ lines content
   print $ solve linedOutput
+  let p2 = solve2 <$> linedOutput
+  print p2
+  print $ sum . concat $ p2
 
 solve :: [[[String]]] -> Int
 solve line =
@@ -17,4 +23,37 @@ solve line =
       )
       line
   where
-    include = (`elem` [2,3,4,7])
+    include = (`elem` [2, 3, 4, 7])
+
+baseMap :: M.Map String Int
+baseMap =
+  M.fromList $
+    zip
+      [ "abcefg",
+        "cf",
+        "acdeg",
+        "acdfg",
+        "bcdf",
+        "abdfg",
+        "abdefg",
+        "acf",
+        "abcdefg",
+        "abcdfg"
+      ]
+      [0 .. 9]
+
+allMappings :: [M.Map Char Char]
+allMappings = fmap (M.fromList . zip "abcdefg") $ permutations "abcdefg"
+
+dtoi :: [Int] -> Int
+dtoi = foldl' (\acc x -> acc * 10 + x) 0
+
+solve2 :: [[String]] -> [Int]
+solve2 [lhs, rhs] = do
+  tryMapping <- allMappings
+  let mapped = fmap $ sort . fmap (tryMapping M.!)
+  let lhs' = mapped lhs
+  let rhs' = mapped rhs
+  guard $ all (`M.member` baseMap) lhs'
+  pure $ dtoi $ (baseMap M.!) <$> rhs'
+solve2 _ = [0]
